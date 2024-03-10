ad5f997d792f37b9f50a4d4ffa266c44436a3752
Leandro H <ndroher+git@proton.me>
2024-03-10 19:55:07 -0300

    feat: add accordion list

--- animais-fantasticos/script.js
+++ animais-fantasticos/script.js
@@ -20,3 +20,22 @@ if (tabMenu.length && tabContent.length) {
     });
   });
 }
+
+// Accordion List
+const accordionDT = document.querySelectorAll(".js-accordion dt");
+const accordionDD = document.querySelectorAll(".js-accordion dd");
+
+if (accordionDD.length && accordionDT.length) {
+  accordionDD[0].classList.add("ativo");
+
+  function accordionList(dt) {
+    dt.classList.toggle("ativo");
+    dt.nextElementSibling.classList.toggle("ativo");
+  }
+
+  accordionDT.forEach((dt) => {
+    dt.addEventListener("click", () => {
+      accordionList(dt);
+    });
+  });
+}
