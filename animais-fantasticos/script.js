fca24d352bc806ef532d03daa323de400ceda313
Leandro H <ndroher+git@proton.me>
2024-03-12 22:26:18 -0300

    perf: check js variables before activating smooth scrolling

--- animais-fantasticos/script.js
+++ animais-fantasticos/script.js
@@ -44,16 +44,18 @@ if (accordionDD.length && accordionDT.length) {
 // Scroll Suave
 const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-function scrollToSection(event) {
-  event.preventDefault();
-  const href = event.currentTarget.getAttribute("href");
-  const section = document.querySelector(href);
-  section.scrollIntoView({
-    behavior: "smooth",
-    block: "start",
+if (linksInternos) {
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
   });
 }
-
-linksInternos.forEach((link) => {
-  link.addEventListener("click", scrollToSection);
-});
