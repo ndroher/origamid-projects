2118acb6276644bae6919643349c0c552c98dab0
Leandro H <ndroher+git@proton.me>
2024-03-12 22:16:35 -0300

    feat: add smooth scrolling to sections

--- animais-fantasticos/script.js
+++ animais-fantasticos/script.js
@@ -40,3 +40,20 @@ if (accordionDD.length && accordionDT.length) {
     });
   });
 }
+
+// Scroll Suave
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute("href");
+  const section = document.querySelector(href);
+  section.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
