6cd1cdb8c26e0bf0e84f1464ba7b0ced07534242
Leandro H <ndroher+git@proton.me>
2024-03-14 17:11:51 -0300

    feat: add Section Animation on Scroll

--- animais-fantasticos/script.js
+++ animais-fantasticos/script.js
@@ -59,3 +59,21 @@ if (linksInternos) {
     link.addEventListener("click", scrollToSection);
   });
 }
+
+// Section Animation on Scroll
+const sections = document.querySelectorAll(".js-scroll");
+
+if (sections.length) {
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      if (sectionTop < window.innerHeight * 0.6) {
+        section.classList.add("ativo");
+      }
+    });
+  }
+
+  animaScroll();
+
+  window.addEventListener("scroll", animaScroll);
+}
